253390c26951620995996203939d3866f8647c25
Jae Lee <jaelee2009@gmail.com>
2020-02-17 10:00:29 -0500

    checkins

---
+++ users/admin.py
@@ -0,0 +1,10 @@
+# users/admin.py
+
+from django.contrib import admin
+from .models import Profile 
+
+# Register your models here.
+
+admin.site.register(Profile) 
+
+
---
+++ users/forms.py
@@ -0,0 +1,28 @@
+# users/forms.py
+
+from django import forms 
+from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm 
+from .models import Profile 
+
+# adds a new "email" field to User
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User 
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image'] 
